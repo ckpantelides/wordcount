99d0b28659f474f516ad38a998340c5781afa44b
CK Pantelides <ckpantelides@gmail.com>
2019-03-30 11:28:43 +0000

    counter now ignores numbers

--- modules/count.js
+++ modules/count.js
@@ -1,21 +1,22 @@
 // word count funtion
 function wordCount(array) {
-
   // instantiate the object to hold the counted words
   var count = {};
   // loop through the array of text
   for (var i = 0; i < array.length; i++) {
-    // for each word, if the count object has a key with that value, add 1 to the
-    // value of the key
-    if (count.hasOwnProperty(array[i])) {
-      count[array[i]] += 1;
+    // ignores numbers
+    if (isNaN(array[i])) {
+      // for each word, if the count object has a key with that value, add 1 to the
+      // value of the key
+      if (count.hasOwnProperty(array[i])) {
+        count[array[i]] += 1;
       } else {
-      // if the count object doesn't hold that word as a key, it's value pair is 1
-      count[array[i]] = 1;
+        // if the count object doesn't hold that word as a key, it's value pair is 1
+        count[array[i]] = 1;
       }
     }
-    // console.log(count);
-    return(count);
+  }
+  return count;
 }
 
 exports.wordCount = wordCount;
